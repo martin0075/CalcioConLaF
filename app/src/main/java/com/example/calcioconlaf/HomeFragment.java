265e2ef2d29923308584089c7d675f08f1f26c02
Davide2805 <davide.urbinati2@studio.unibo.it>
2022-07-29 14:21:33 +0200

    commit

--- app/src/main/java/com/example/calcioconlaf/HomeFragment.java
+++ app/src/main/java/com/example/calcioconlaf/HomeFragment.java
@@ -10,7 +10,7 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link HomeFragment#} factory method to
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
