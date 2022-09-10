01b6f8ff1e07c7294dc798961ba66a0d8ee5683e
martin0075 <83312013+martin0075@users.noreply.github.com>
2022-09-10 14:25:41 +0200

    Comleted: HomePage, LoginPage, RegisterPage

--- app/src/main/java/com/example/calcioconlaf/GameStadium/LobbyActivity.java
+++ app/src/main/java/com/example/calcioconlaf/GameStadium/LobbyActivity.java
@@ -1,15 +1,22 @@
 package com.example.calcioconlaf.GameStadium;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.example.calcioconlaf.GameActivity;
 import com.example.calcioconlaf.HomeFragment;
 import com.example.calcioconlaf.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import org.w3c.dom.Text;
 
@@ -25,9 +32,7 @@ public class LobbyActivity extends AppCompatActivity {
     String usernameLobby;
     String indexLobby;
     LobbyActivity lobbyActivity=LobbyActivity.this;
-    int a;
-    int b;
-    int c;
+    String indice;
     Random r=new Random();
     ArrayList<Quiz> domande=new ArrayList<Quiz>();
     DomandeThread domandeThread;
@@ -70,6 +75,77 @@ public class LobbyActivity extends AppCompatActivity {
             }
         }.start();
     }
+
+    @Override
+    protected void onStop() {
+        Log.v("STOP","STOP");
+        super.onStop();
+        DatabaseReference ref=database.getReference();
+        DatabaseReference utentiRef = ref.child("GameStadium").child(indexLobby).child("utenti");
+        utentiRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds:snapshot.getChildren()){
+                    if(ds.child("username").getValue().equals(username)){
+                        indice=ds.getKey();
+                        utentiRef.child(indice).child("stoppato").setValue("true");
+                        //Intent intent = new Intent(quizStadium, GameActivity.class);
+                        //intent.putExtra("Username", username);
+                        //quizStadium.startActivity(intent);
+                    }else{
+                        indice=ds.getKey();
+                        utentiRef.child(indice).child("stoppato").setValue("false");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Intent intent = new Intent(lobbyActivity, GameActivity.class);
+        intent.putExtra("Username", username);
+        lobbyActivity.startActivity(intent);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        DatabaseReference ref=database.getReference();
+        DatabaseReference utentiRef = ref.child("GameStadium").child(indexLobby).child("utenti");
+        utentiRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds:snapshot.getChildren()){
+                    if(ds.child("username").getValue().equals(username)){
+                        indice=ds.getKey();
+                        utentiRef.child(indice).child("stoppato").setValue("true");
+                        //Intent intent = new Intent(quizStadium, GameActivity.class);
+                        //intent.putExtra("Username", username);
+                        //quizStadium.startActivity(intent);
+                    }else{
+                        indice=ds.getKey();
+                        utentiRef.child(indice).child("stoppato").setValue("false");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+
     @Override
     public void onBackPressed() {
 
--- app/src/main/java/com/example/calcioconlaf/GameTransfer/LobbyActivityTransfer.java
+++ app/src/main/java/com/example/calcioconlaf/GameTransfer/LobbyActivityTransfer.java
@@ -1,15 +1,22 @@
 package com.example.calcioconlaf.GameTransfer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.widget.TextView;
 
+import com.example.calcioconlaf.GameActivity;
 import com.example.calcioconlaf.HomeFragment;
 import com.example.calcioconlaf.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -24,6 +31,7 @@ public class LobbyActivityTransfer extends AppCompatActivity {
     String indexLobbyTransfer;
     LobbyActivityTransfer lobbyActivity=LobbyActivityTransfer.this;
     Random r=new Random();
+    String indice;
     ArrayList<QuizTransfer> domande=new ArrayList<QuizTransfer>();
     HomeFragment home=new HomeFragment();
     LobbyTransferThread lobbyTransferThread;
@@ -63,6 +71,74 @@ public class LobbyActivityTransfer extends AppCompatActivity {
 
             }
         }.start();
+    }
+    @Override
+    protected void onStop() {
+        Log.v("STOP","STOP");
+        super.onStop();
+        DatabaseReference ref=database.getReference();
+        DatabaseReference utentiRef = ref.child("GameStadium").child(indexLobbyTransfer).child("utenti");
+        utentiRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds:snapshot.getChildren()){
+                    if(ds.child("username").getValue().equals(usernameLobbyTransfer)){
+                        indice=ds.getKey();
+                        utentiRef.child(indice).child("stoppato").setValue("true");
+                        //Intent intent = new Intent(quizStadium, GameActivity.class);
+                        //intent.putExtra("Username", username);
+                        //quizStadium.startActivity(intent);
+                    }else{
+                        indice=ds.getKey();
+                        utentiRef.child(indice).child("stoppato").setValue("false");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Intent intent = new Intent(lobbyActivity, GameActivity.class);
+        intent.putExtra("Username", usernameLobbyTransfer);
+        lobbyActivity.startActivity(intent);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        DatabaseReference ref=database.getReference();
+        DatabaseReference utentiRef = ref.child("GameStadium").child(indexLobbyTransfer).child("utenti");
+        utentiRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot ds:snapshot.getChildren()){
+                    if(ds.child("username").getValue().equals(usernameLobbyTransfer)){
+                        indice=ds.getKey();
+                        utentiRef.child(indice).child("stoppato").setValue("true");
+                        //Intent intent = new Intent(quizStadium, GameActivity.class);
+                        //intent.putExtra("Username", username);
+                        //quizStadium.startActivity(intent);
+                    }else{
+                        indice=ds.getKey();
+                        utentiRef.child(indice).child("stoppato").setValue("false");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
     @Override
     public void onBackPressed() {
